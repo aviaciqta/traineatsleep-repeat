b1fa69a935be6ecb91d1f4fc32b8179547826c37
aviaciqta <aviaciqta@gmail.com>
2023-04-08 10:33:42 +0300

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,23 +6,20 @@ const About = () => {
       <div className="col-md-12">
         <div className="row mx-0">
           <p>
-            Contrary to popular belief, Lorem Ipsum is not simply random text.
-            It has roots in a piece of classical Latin literature from 45 BC,
-            making it over 2000 years old. Richard McClintock, a Latin professor
-            at Hampden-Sydney College in Virginia, looked up one of the more
-            obscure Latin words, consectetur, from a Lorem Ipsum passage, and
-            going through the cites of the word in classical literature,
-            discovered the undoubtable source. Lorem Ipsum comes from sections
-            1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes
-            of Good and Evil) by Cicero, written in 45 BC. This book is a
-            treatise on the theory of ethics, very popular during the
-            Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit
-            amet..", comes from a line in section 1.10.32. The standard chunk of
-            Lorem Ipsum used since the 1500s is reproduced below for those
-            interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et
-            Malorum" by Cicero are also reproduced in their exact original form,
-            accompanied by English versions from the 1914 translation by H.
-            Rackham.
+          Пътят към успешната промяна започва от тук. Направи го по правилния начин!
+          Влагаш много време и усилия, за да изгориш излишните мазнини и да оформиш мускулатурата и тялото си, но БЕЗ УСПЕХ? Напредъкът е минимален и трудно постижим? Усещаш, че има проблем, но не можеш да напипаш точно къде и какъв е той, за да го отстраниш? Резултатите са посредствени, мотивацията спада, чувстваш се ОБЕЗКУРАЖЕН…
+          И я пиша като човек, който сам е изминал същия трънлив път. С много дупки, спънки и препятствия към успеха. Пиша я не за да разкажа историята си, а за да споделя опита и знанията си, натрупани през годините, с надеждата да помогна и на теб и да те насоча в правилна посока.
+          Най-честите причини за неуспех и как да ги избегнеш
+Когато говорим за фитнес и здравословно хранене – опита показва, че мнозинството от начинаещи в сферата допускат едни и същи често срещани грешки. Всъщност, едва ли „грешки“ е точната дума. За мен това са по-скоро ПРИЧИНИ ЗА НЕУСПЕХА. И когато знаем за тях – по-лесно можем да ги преодолеем.
+
+Основните причини за неуспех в диетата и тренировките са три:
+
+Липса на знания и неразбиране на материята;
+Липса на достатъчно мотивация;
+Липса на постоянство и отговорност;
+За над 10 години професионален път като треньор мога да кажа, че в 90% от случаите, причината за неуспеха на хората влиза в една от тези три категории. Нека ги разгледаме по отделно:
+
+            
           </p>
         </div>
       </div>
